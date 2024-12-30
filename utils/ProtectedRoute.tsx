4df74cdd10fdb9210c624534152b6ff1e69033e4
Tarkan Zarrouk <83801755+LarryTheFatCat@users.noreply.github.com>
2024-12-29 23:57:38 -0500

    cannot find children bugfix

--- utils/ProtectedRoute.tsx
+++ utils/ProtectedRoute.tsx
@@ -1,16 +1,23 @@
 import { Spinner } from "@nextui-org/react";
-import { useAuth } from "./CentralAuth"
+import { useAuth } from "./CentralAuth";
 import { useRouter } from "next/router";
 
 const ProtectedRoute = ({ children }) => {
-    const { user, loading } = useAuth();
-    const router = useRouter();
-    if(loading) {
-        return <div><Spinner /></div>;
-    }
-    if(!user) {
-        router.push("/login")
-    }
-    return children;
-}
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  if (loading) {
+    return (
+      <div>
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (!user) {
+    return router.push("/login");
+  }
+
+  return children;
+};
 export default ProtectedRoute;
